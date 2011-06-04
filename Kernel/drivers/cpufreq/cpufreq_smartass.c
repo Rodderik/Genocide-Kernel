74851bce93c8acc51a00d2fcddce1aee864bedac
nubecoder <nubecoder@gmail.com>
2011-06-04 07:18:29 -0400

    Changes to DEFAULT_AWAKE_MIN_FREQ in smartass governor.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/drivers/cpufreq/cpufreq_smartass.c
+++ Kernel/drivers/cpufreq/cpufreq_smartass.c
@@ -110,7 +110,7 @@ static unsigned int sleep_wakeup_freq;
  * go below this frequency.
  * Set awake_min_freq=0 to disable this behavior.
  */
-#define DEFAULT_AWAKE_MIN_FREQ 518400
+#define DEFAULT_AWAKE_MIN_FREQ CONFIG_CPU_HUMMINGBIRD_MIN_FREQ
 static unsigned int awake_min_freq;
 
 /*
