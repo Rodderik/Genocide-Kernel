843e0b506353e62dbb0c125789d629c244667c59
Rodderik <usmccool@gmail.com>
2011-06-10 11:47:23 -0400

    ClockworkMod 3.1.0.1 Purple Edition Added support for CWM 3.1.0.1 Working redirector Rom manager compatibility Busybox handed by recovery binary instead of a seperate binary Other misc tweaks to initramfs
    Big thanks to DRockstar and all his work for making the new CWM and other scripts possible

--- EC05_Genocide_initramfs/sbin/custom_init.sh
+++ EC05_Genocide_initramfs/sbin/custom_init.sh
@@ -1,46 +1,32 @@
 #!/system/bin/sh
 # Originally written by tanimn for Twilight Zone Kernel 1.0.3
 # Modified by Rodderik for Genocide Kernel
-
-# Do Customizations
+# CWM compatibility and keytimer support by DRockstar
 
 # Remount filesystems RW
 
-busybox mount -o remount,rw /
-busybox mount -o remount,rw /system
+/sbin/busybox mount -o remount,rw /dev/block/stl9 /system
+/sbin/busybox mount -o remount,rw / /
 
-# Install BusyBox 1.18
-
-/sbin/busybox --install -s /system/bin
+# Install busybox
+/sbin/busybox mkdir /bin
+/sbin/busybox --install -s /bin
+rm -rf /system/xbin/busybox
+ln -s /sbin/busybox /system/xbin/busybox
+rm -rf /res
 sync
 
-# Lock the CPU down til SetCPU or such can set it.
+# Fix permissions in /sbin, just in case
+chmod 755 /sbin/*
 
+# Lock the CPU down til SetCPU or such can set it.
 echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq
 
 # Switch to Conservative CPU governor after bootup
-
 echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-# Enable init.d support
-
-if [ -d /system/etc/init.d ]
-then
-	logwrapper busybox run-parts /system/etc/init.d
-fi
-sync
-
-# keyboard patch sysfs call 5 for snappy keyboard performance (range: 1-16 default: 7)
-# Thanks DRockstar
-
-if [ ! -f "/data/local/timer_delay" ]; then
-  echo 5 > /data/local/timer_delay
-fi
-cat /data/local/timer_delay > /sys/devices/platform/s3c-keypad/timer_delay
-
 # Fix screwy ownerships
-
 for blip in conf default.prop fota.rc init init.goldfish.rc init.rc init.smdkc110.rc lib lpm.rc modules recovery.rc res sbin
 do
 	chown root.system /$blip
@@ -50,16 +36,36 @@ done
 chown root.system /lib/modules/*
 chown root.system /res/images/*
 
+# Enable init.d support
+if [ -d /system/etc/init.d ]
+then
+	logwrapper busybox run-parts /system/etc/init.d
+fi
+sync
 
-chmod 6755 /sbin/su
+# keyboard patch sysfs call 5 for snappy keyboard performance (range: 1-16 default: 7)
+if [ ! -f "/data/local/timer_delay" ]; then
+  echo 5 > /data/local/timer_delay
+fi
+cat /data/local/timer_delay > /sys/devices/platform/s3c-keypad/timer_delay
+
+# setup tmpfs for su
+mkdir /sdx
+mount -o size=30k -t tmpfs tmpfs /sdx
+cat /sbin/su > /sdx/su
+chmod 6755 /sdx/su
+# establish root in common system directories for 3rd party applications
 rm /system/bin/su
 rm /system/xbin/su
-ln -s /sbin/su /system/bin/su
-ln -s /sbin/su /system/xbin/su
+rm /system/bin/jk-su
+ln -s /sdx/su /system/bin/su
+ln -s /sdx/su /system/xbin/su
+# remove su in problematic locations
+rm -rf /bin/su
+rm -rf /sbin/su
 
 #setup proper passwd and group files for 3rd party root access
 # Thanks DevinXtreme
-
 if [ ! -f "/system/etc/passwd" ]; then
 	echo "root::0:0:root:/data/local:/system/bin/sh" > /system/etc/passwd
 	chmod 0666 /system/etc/passwd
@@ -70,7 +76,6 @@ if [ ! -f "/system/etc/group" ]; then
 fi
 
 #Check and setup Superuser if missing
-
  if [ ! -f "/system/app/Superuser.apk" ] && [ ! -f "/data/app/Superuser.apk" ] && [[ ! -f "/data/app/com.noshufou.android.su"* ]]; then
 	if [ -f "/system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk" ]; then
 		rm /system/app/Asphalt5_DEMO_ANMP_Samsung_D700_Sprint_ML.apk
@@ -78,13 +83,12 @@ fi
 	if [ -f "/system/app/FreeHDGameDemos.apk" ]; then
 		rm /system/app/FreeHDGameDemos.apk
 	fi
- 	busybox cp /support/Superuser.apk /system/app/Superuser.apk
+ 	busybox cp /sbin/Superuser.apk /system/app/Superuser.apk
  fi
 
 # fix busybox DNS while system is read-write
-
 if [ ! -f "/system/etc/resolv.conf" ]; then
-	echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
+	echo "nameserver 8.8.8.8" > /system/etc/resolv.conf
 	echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
 fi 
 sync
@@ -97,6 +101,6 @@ touch /system/bin/profile
 chmod 644 /system/bin/profile
 
 # remount read only and continue
-busybox mount -o remount,ro /
-busybox mount -o remount,ro /system
+mount -o remount,ro /dev/stl9 /system
+mount -o remount,ro / /
 
---
+++ EC05_Genocide_initramfs/sbin/recovery.sh
@@ -0,0 +1,36 @@
+#!/sbin/sh
+# DRockstar Clean Kernel recovery.sh called from recovery.rc and fota.rc
+
+/sbin/busybox mount -o remount,rw / /
+
+# Install recovery busybox to /sbin
+/sbin/busybox ls /res/sbin | while read line
+do
+  /sbin/busybox mv -f /res/sbin/$line /sbin/$line
+done
+rmdir /res/sbin
+
+# Remove unneccessary files used in normal boot
+for file in Superuser.apk su keytimer
+do
+  rm -rf /sbin/$file
+done
+
+# Fix permissions in /sbin, just in case
+/sbin/busybox chmod 755 /sbin/*
+
+# Fix screwy ownerships
+for blip in conf default.prop fota.rc init init.goldfish.rc init.rc init.smdkc110.rc lib lpm.rc modules recovery.rc res sbin
+do
+  chown root.system /$blip
+  chown root.system /$blip/*
+done
+
+chown root.system /lib/modules/*
+chown root.system /res/images/*
+
+mkdir /etc
+cp /res/etc/recovery.fstab /etc/recovery.fstab
+sync
+/sbin/recovery
+
